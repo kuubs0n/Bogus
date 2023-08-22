c50fe31cc01760c6ce19077be2a6d96a8cc38134
Jakub Krawczyk <j.krawczyk@curulis.pl>
2023-08-22 21:57:10 +0200

    Init

---
+++ BogusExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using Bogus.DataSets;
+using BogusExample;
+
+Randomizer.Seed = new Random(123456789);
+
+var testUsers = new Faker<User>()
+    .RuleFor(u => u.Id, f => Guid.NewGuid())
+    .RuleFor(u => u.FirstName, f => f.Name.FirstName())
+    .RuleFor(u => u.LastName, f => f.Name.LastName())
+    .RuleFor(u => u.Birthday, f => f.Date.BetweenDateOnly(DateOnly.Parse("1990, 01, 01"), DateOnly.Parse("2022, 01, 01")))
+    .RuleFor(u => u.IsActivated, f => f.Random.Bool(0.8f));
+    
+var results = testUsers.GenerateLazy(100).ToList();
---
+++ BogusExample/User.cs
@@ -0,0 +1,10 @@
+﻿namespace BogusExample;
+
+public record User
+{
+    public Guid Id { get; set; }
+    public string FirstName  { get; set; }
+    public string LastName { get; set; }
+    public DateOnly Birthday { get; set; }
+    public bool IsActivated { get; set; }
+}
